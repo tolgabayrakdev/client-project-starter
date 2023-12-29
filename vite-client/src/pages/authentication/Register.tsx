0e2e32aba376bd552c5556282a257bd47f18720e
tolgabayrakdev <bayraktolga28@gmail.com>
2023-12-29 09:29:10 +0300

    register page message

--- vite-client/src/pages/authentication/Register.tsx
+++ vite-client/src/pages/authentication/Register.tsx
@@ -1,16 +1,57 @@
 import { Card } from 'antd';
-import { Button, Form, Input } from 'antd';
-import { Link } from 'react-router-dom';
+import { Button, Form, Input, message } from 'antd';
+import { Link, useNavigate } from 'react-router-dom';
 
-type Props = {};
+export default function Register() {
+  const [messageApi, contextHolder] = message.useMessage();
+  const navigate = useNavigate();
 
-export default function Register({}: Props) {
-  const onFinish = (values: any) => {
-    console.log('Success:', values);
+  const registerAlredyUsedError = () => {
+    messageApi.open({
+      type: 'error',
+      content: 'Username or email already used!',
+    });
+  };
+
+  const loginSuccessfulMessage = () => {
+    messageApi.open({
+      type: 'success',
+      content: 'Username or email already used!',
+    });
+  };
+
+  const onFinish = async (values: {
+    username: string;
+    email: string;
+    password: string;
+  }) => {
+    try {
+      const res = await fetch('http://localhost:8000/api/v1/auth/register', {
+        method: 'POST',
+        credentials: 'include',
+        headers: {
+          'Content-type': 'application/json',
+        },
+        body: JSON.stringify({
+          username: values.username,
+          email: values.email,
+          password: values.password,
+        }),
+      });
+      if (res.status === 201) {
+        loginSuccessfulMessage();
+        navigate('/authentication/login');
+      } else if (res.status === 500) {
+        registerAlredyUsedError();
+      }
+    } catch (e) {
+      console.log(e);
+    }
   };
 
   return (
     <div>
+      {contextHolder}
       <h4 className="text-center text-xl mb-1">Register here</h4>
       <Card className="bg-gray-50" style={{ width: 400, height: 280 }}>
         <Form onFinish={onFinish}>
@@ -25,6 +66,7 @@ export default function Register({}: Props) {
               {
                 required: true,
                 message: 'Please input your username!',
+                min: 3,
               },
             ]}
           >
